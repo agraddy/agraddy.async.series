81f0385051f6ba5f58d6053aac1fab765324706e
Anthony Graddy <anthony@locationq.com>
2017-01-21 08:31:44 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -3,6 +3,71 @@ var tap = require('agraddy.test.tap')(__filename);
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+start();
+
+function start() {
+	var list = [];
+	mod([
+		function(cb) {
+			list.push(1);
+			cb();
+		},
+		function(cb) {
+			list.push(2);
+			cb();
+		},
+		function(cb) {
+			list.push(3);
+			cb();
+		}
+	], function(err) {
+		tap.assert.deepEqual(list, [1,2,3], 'Should be equal.');
+		endEarly();
+	});
+}
+
+function endEarly() {
+	var list = [];
+	mod([
+		function(cb) {
+			list.push(1);
+			cb();
+		},
+		function(cb) {
+			list.push(2);
+			cb(new Error('End early'), 'one', 'two');
+		},
+		function(cb) {
+			list.push(3);
+			cb();
+		}
+	], function(err) {
+		tap.assert.equal(err.message, 'End early', 'An error should short circuit.');
+		tap.assert.deepEqual(list, [1,2], 'Should be equal.');
+		async();
+	});
+}
+
+
+function async() {
+	var i;
+	var list = [];
+	var func = function(cb) { cb() };
+	var order;
+	for(i = 0; i < 100; i++) {
+		list.push(func);
+	}
+
+	order = 'before';
+	mod(list, function(err) {
+		tap.assert.equal(order, 'after', 'Make sure it is actually async.');
+		end();
+	});
+	order = 'after';
+}
+
+function end() {
+}
+
 
 
